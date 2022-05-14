2c5b6a5be31c8c4aaaaaf7537540abf89a99d73d
Min Khant Saw <74578345+min-khant-saw@users.noreply.github.com>
2022-05-14 10:35:22 +0630

    new

--- about.js
+++ about.js
@@ -1,6 +1,12 @@
 /* Companys */
 const company = document.querySelector(".company");
-const companys = ["", "", "", "", ""];
+const companys = [
+  "https://uptimerobot.com/assets/images/godaddy-logo.svg",
+  "https://uptimerobot.com/assets/images/nasa.svg",
+  "https://uptimerobot.com/assets/images/moodys-logo.svg",
+  "https://uptimerobot.com/assets/images/ibm-logo.svg",
+  "https://uptimerobot.com/assets/images/expedia-logo.svg",
+];
 
 for (let y = 0; y < companys.length; y++) {
   const img = document.createElement("img");
